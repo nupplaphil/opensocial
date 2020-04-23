7fa87a769d8cf78cba9fe6cffebbc85f7d2c3f03
Philipp <admin@philipp.info>
2020-04-23 23:28:15 +0200

    WIP

--- src/modules/user/useCases/getInbox/index.ts
+++ src/modules/user/useCases/getInbox/index.ts
@@ -1,6 +1,6 @@
 import {GetInboxController} from './getInboxController';
-import {GetInboxUseCase} from "@modules/user/useCases/getInbox/getInboxUseCase";
-import {UserRepository} from "@modules/user/infra/knex/UserRepository";
+import {GetInboxUseCase} from './getInboxUseCase';
+import {UserRepository} from '../../infra/knex/UserRepository';
 import {knex} from '@db';
 
 const userRepo = new UserRepository(knex);
